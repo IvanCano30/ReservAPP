176daf25de453b59cfcd7a6582c2f14ba46181a0
ivan cano <ivan.cano.cv@gmail.com>
2022-08-24 18:09:49 -0300

    reservAPP con alertas

--- js/ReservAPP1.js
+++ js/ReservAPP1.js
@@ -1,16 +1,22 @@
-//reserva
-let reserva = []
+
+
+//boton para que cambie titulo
+
 let reservaOk = false;
 const titulo = document.getElementById("titulo");
-const parrafoApp = document.getElementById("parrafo")
-const botonApp = document.getElementById("boton");
 const contenidoTitulo = "Bienvenido a ReservAPP";
 const contenidoTitulo2 = "Gracias por usar ReservAPP";
+const parrafoApp = document.getElementById("parrafo")
+const botonApp = document.getElementById("boton");
+
+//const cambio de parrafo con setInterval
 const cambioParrafo1 = "Clasificanos en la APP STORE";
 const cambioParrafo2 = "Siguenos en nuestras redes sociales";
 
-cambiarTexto(parrafoApp, cambioParrafo1); // se inicia con texto1
 
+//parrafo con interval
+
+cambiarTexto(parrafoApp, cambioParrafo1); // se inicia con texto1
 
 const intervalo = setInterval(()=>{
 	if(parrafoApp.innerText == cambioParrafo1){
@@ -20,9 +26,8 @@ const intervalo = setInterval(()=>{
 }
 },2000);
 
+//boton con cambio de titulo
 
-
-//boton
 botonApp.addEventListener("click", ()=>{
     let info = titulo.innerText;
     if (info === contenidoTitulo){
@@ -38,76 +43,4 @@ function cambiarTexto(id, texto){
 }
 
 cambiarTexto(titulo, contenidoTitulo);
-
-//crea una reserva, hora, mesa y comida.
-class Mireserva {
-    constructor( reserva , horario , mesa){
-        this.reserva = reserva
-        this.horario = horario
-        this.mesa = mesa
-        
-    }
-}
-
-const reserva1 = new Mireserva ("Vrinda" , ["Mañana", "Tarde" , "Noche"], ["adentro" , "afuera" , "terraza"])
-const reserva2 = new Mireserva ("Rockefeller" , ["Mañana", "Tarde" , "Noche"], ["adentro" , "afuera" , "patio de comida"])
-
-//selecciona reserva
-const seleccionarBar = () =>{
-    const seleccionUsuario = prompt("Elige la reserva que quieren entre los bares: Vrinda  Rockefeller")
-    if(seleccionUsuario != null){
-
-    seleccionUsuario.toLowerCase();
-    switch(seleccionUsuario) {
-        case "vrinda":
-            console.log("Elegiste Vrinda")
-            reserva.push(editarReservaSeleccionada(reserva1))
-            break
-        case "rockefeller":
-            console.log("Elegiste Rockefeller")
-            reserva.push(editarReservaSeleccionada(reserva2))
-            break
-        default:
-            console.log("Elegir un bar correcto")
-            break
-    }
-    
-    if (confirm("¿Desea hacer reserva en otro bar y horario?")){
-    seleccionarBar()
-    }
-}
-}
-//caracteristicas de la reserva
-const editarReservaSeleccionada = (ReservaSeleccionada) => {
-    const reservaElegida = {
-        reserva : ReservaSeleccionada.reserva,
-        horario : "",
-        mesa : "",
-        cantidadPersonas: 1,
-    }
-    reservaElegida.mesa = prompt("Seleccione una ubicacion para su reserva: " + ReservaSeleccionada.mesa.join(" - ") ).toLowerCase()
-    reservaElegida.cantidadPersonas = Number(prompt("¿Para cuantas personas reserva? "))
-    reservaElegida.horario = prompt("Seleccione un horario dentro de los que tenemos disponibles: " + ReservaSeleccionada.horario.join(" - ")).toLowerCase()
-    reservaOk = true
-    return reservaElegida
-}
-
-// su reserva es..
-const suReserva = () => {
-    let suReserva = ""
-    for (const producto of reserva) {
-        suReserva += " Bar: " + producto.reserva + " Turno de Servicio: " + producto.horario + " Ubicacion: " + producto.mesa + " Cantidad de personas: " + producto.cantidadPersonas
-    }
-    
-    return suReserva
-}
-
-// ejecucion
-seleccionarBar()
-if(reservaOk == true){
-    alert('Gracias por reservar en ReservAPP, su reserva es:' + suReserva())
-    console.log('Gracias por reservar en ReservAPP, su reserva en:' + suReserva())
-    reservaOk = false
-    cambiarTexto(titulo, contenidoTitulo2)
-}
 
---
+++ js/formularioReserva.js
@@ -0,0 +1,105 @@
+//array reservas
+let reservas = []
+//alerta de bienvenida
+swal("Bienvenido a reservAPP!");
+window.onload = function(){
+    
+   // Recuperar el localstorage
+    if(localStorage.getItem("reserva").length > 0){
+        reservas = JSON.parse(localStorage.getItem("reserva")); 
+        
+    }  
+
+}
+// mostrar mensaje cuando se use el input
+function showMessage(input, message, type) {
+	// obtener un elemento y hacer un mensanje
+	const msg = input.parentNode.querySelector("small");
+	msg.innerText = message;
+	// input class
+	input.className = type ? "success" : "error";
+	return type;
+}
+
+function showError(input, message) {
+	return showMessage(input, message, false);
+}
+
+function showSuccess(input) {
+	return showMessage(input, "", true);
+}
+
+function hasValue(input, message) {
+	if (input.value.trim() === "") {
+		return showError(input, message);
+	}
+	return showSuccess(input);
+}
+
+function validateEmail(input, requiredMsg, invalidMsg) {
+	// checkeo del if si no es invalido
+	if (!hasValue(input, requiredMsg)) {
+		return false;
+	}
+	// validacion de formato email
+	const emailRegex =
+		/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+	const email = input.value.trim();
+	if (!emailRegex.test(email)) {
+		return showError(input, invalidMsg);
+	}
+	return true;
+}
+
+const form = document.querySelector("#signup");
+
+const NAME_REQUIRED = "por favor ingrese su nombre y apellido";
+const EMAIL_REQUIRED = "por favor ingrese su email";
+const EMAIL_INVALID = "Please enter a correct email address format";
+
+form.addEventListener("submit", function (event) {
+	// presentacion de la funcion
+	event.preventDefault();
+
+	// valdacion del formulario
+	let nameValid = hasValue(form.elements["name"], NAME_REQUIRED);
+	let emailValid = validateEmail(form.elements["email"], EMAIL_REQUIRED, EMAIL_INVALID);
+    // si es invalido, enviar al form
+	if (nameValid && emailValid) {
+		let nombre = form.elements["name"].value;
+        let email = form.elements["email"].value;
+        let cantPersonas = form.elements["personas"].value
+        let ubicacionMesa = form.elements["ubicacionMesa"].value
+        let horario = form.elements["horario"].value
+        let bar = form.elements["opcionBares"].value
+
+        let nuevaReserva = new miReserva (bar , horario, ubicacionMesa, cantPersonas, nombre, email)
+        reservas.push(nuevaReserva)
+        //alerta de success
+        swal("Gracias por reservar!", nombre ,  "success");
+    }
+    //localstorage
+    localStorage.setItem ("reserva", JSON.stringify(reservas))
+    
+    
+});
+//crea una reserva, hora, mesa y comida.
+class miReserva {
+    constructor( bar , horario , ubicacionMesa, cantPersonas, nombre, email){
+        this.bar = bar
+        this.horario = horario
+        this.ubicacionMesa = ubicacionMesa
+        this.cantPersonas = cantPersonas
+        this.nombre = nombre
+        this.email = email        
+    }
+}
+
+
+
+
+
+
+
+
